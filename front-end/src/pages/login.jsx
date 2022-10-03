c78f0757d4737a27d4f31f6d5e5daceb3c57639d
DeclanElk <delkerton2001@gmail.com>
2022-10-03 15:26:47 +1000

    adding login handling

--- front-end/src/pages/login.jsx
+++ front-end/src/pages/login.jsx
@@ -1,21 +1,66 @@
 import React from 'react';
+import axios from 'axios';
 
 export default function Login() {
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+
+    //Submit function
+    function submit(event) {
+        console.log('test')
+        axios
+        .post('http://localhost:1337/api/auth/local', {
+            identifier: email,
+            password: password
+        })
+        .then(response => {
+            console.log("Successfully logged in")
+        })
+        .catch(error => {
+            console.log("An error has occurred:", error.response)
+        })
+        event.preventDefault();
+    }
+
     return (
         <div className='container text-center mt-5 p-5'>
             <h1>Login</h1>
-            <form>
+            <form onSubmit={submit}>
                 <div className='form-group row d-flex justify-content-center'>
                     <div className="col-md-7 mb-3 mt-5">
-                        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email" />
+                        <input 
+                        type="email" 
+                        className="form-control" 
+                        id="email" 
+                        placeholder="Enter email" 
+                        name="email" 
+                        value={email}
+                        onChange={(event) => {
+                            setEmail(event.target.value)
+                        }}/>
                     </div>
                 </div>
                 <div className='form-group row d-flex justify-content-center'>
                     <div className="col-md-7 mb-5">
-                        <input type="password" class="form-control" id="password" placeholder="Enter password" name="password" />
+                    <input 
+                        type="password" 
+                        className="form-control" 
+                        id="password" 
+                        placeholder="Enter password" 
+                        name="password"
+                        value={password}
+                        onChange={(event) => {
+                            setPassword(event.target.value)
+                        }}/>
                     </div>
                 </div>
-                <button type="submit" class="btn btn-primary px-4 py-2">Submit</button>
+                <button 
+                type="submit" 
+                onClick={submit} 
+                className="btn btn-primary px-4 py-2" 
+                value="Submit">
+                    Submit
+                </button>
             </form>
         </div>
     )
