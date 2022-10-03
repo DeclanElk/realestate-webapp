5fff41841ee052198ea0c88451b884d42a8cf8b6
DeclanElk <delkerton2001@gmail.com>
2022-10-03 13:16:58 +1000

    Adding logo and nav bar

--- front-end/src/components/navigation.jsx
+++ front-end/src/components/navigation.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function NavigationBar() {
     return (
-        <nav class="navbar navbar-expand-sm bg-primary">
+        <nav class="navbar navbar-expand-sm bg-dark">
             <ul class="navbar-nav">
                 <li class="nav-item">
                     <a href="/" class="nav-link text-light">Home</a>
