997ed5ffc30e0f83df46588ad0230ef4a9ed63cd
DeclanElk <delkerton2001@gmail.com>
2022-10-19 18:26:15 +1000

    Updating home background image

--- front-end/src/pages/home.jsx
+++ front-end/src/pages/home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
-import Select from 'react-select'
+import Select from 'react-select';
+
+import background from '../assets/background.jpg';
 
 export default function Home() {
     const [search, setSearch] = React.useState("");
@@ -46,9 +48,9 @@ export default function Home() {
             <div
             className='bg-image py-5'
             style={{
-                backgroundImage: 'url(https://images.unsplash.com/photo-1495442358998-961b69f45703?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80)',
+                backgroundImage: `url(${background})`,
                 backgroundSize: 'cover',
-                backgroundPosition: 'center'
+                backgroundPosition: '50% 25%'
                 }}>
                 <h1 className='display pt-5 pb-3 text-center font-weight fw-light'>
                     Don't wait to find your dream property
