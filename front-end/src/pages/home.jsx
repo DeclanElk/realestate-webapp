81ed1763211313b41c7c389b17048f8f3b3789a2
DeclanElk <delkerton2001@gmail.com>
2022-10-19 19:08:13 +1000

    Adding icons

--- front-end/src/pages/home.jsx
+++ front-end/src/pages/home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import Select from 'react-select';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBed, faBath, faCar } from '@fortawesome/free-solid-svg-icons'
 
 import background from '../assets/background.jpg';
 
@@ -56,54 +58,70 @@ export default function Home() {
                     Don't wait to find your dream property
                 </h1>
                 <form onSubmit={submit}>
-                    <div className='form-group row d-flex justify-content-center'>
-                        <div className="col-md-7 mt-4 mb-4 d-flex">
-                            <input 
-                            type="text" 
-                            className="form-control me-2" 
-                            id="search" 
-                            placeholder="Enter search" 
-                            name="search" 
-                            value={search}
-                            onChange={(event) => {
-                                setSearch(event.target.value)
-                            }}/>
-                            <button 
-                            type="submit" 
-                            onClick={submit} 
-                            className="btn btn-primary px-4 py-2 btn-dark"
-                            value="Submit">
-                                Search
-                            </button>
+                    <div className='container'>
+                        <div className='form-group row d-flex justify-content-center'>
+                            <div className="col-lg-7 mt-4 mb-4 d-flex">
+                                <input 
+                                type="text" 
+                                className="form-control me-2" 
+                                id="search" 
+                                placeholder="Enter search" 
+                                name="search" 
+                                value={search}
+                                onChange={(event) => {
+                                    setSearch(event.target.value)
+                                }}/>
+                                <button 
+                                type="submit" 
+                                onClick={submit} 
+                                className="btn btn-primary px-4 py-2 btn-dark"
+                                value="Submit">
+                                    Search
+                                </button>
+                            </div>
                         </div>
-                        <div className="col-md-7 mt-4 mb-4 d-flex">
-                        <Select 
-                        className="" 
-                        options={bedOptions}
-                        value={{value: selectedBeds, label: selectedBeds}}
-                        onChange={(event) => {
-                            setSelectedBeds(event.value)
-                            console.log(selectedBeds)
-                        }}
-                        />
-                        <Select 
-                        className="" 
-                        options={bathOptions}
-                        value={{value: selectedBaths, label: selectedBaths}}
-                        onChange={(event) => {
-                            setSelectedBaths(event.value)
-                            console.log(selectedBaths)
-                        }}
-                        />
-                        <Select 
-                        className="" 
-                        options={garageOptions}
-                        value={{value: selectedGarages, label: selectedGarages}}
-                        onChange={(event) => {
-                            setSelectedGarages(event.value)
-                            console.log(selectedGarages)
-                        }}
-                        />
+                        <div className='form-group row d-flex justify-content-center'>
+                            <div className="col-lg-7 mt-4 mb-4 d-flex align-items-center">
+                                <label htmlFor="bedSelect" className='me-2 bg-light p-1 rounded'>
+                                    <FontAwesomeIcon icon={faBed} className="text-dark" size="xl"/>
+                                </label>
+                                <Select 
+                                id="bedSelect"
+                                className="ms-2 me-4" 
+                                options={bedOptions}
+                                value={{value: selectedBeds, label: selectedBeds}}
+                                onChange={(event) => {
+                                    setSelectedBeds(event.value)
+                                    console.log(selectedBeds)
+                                }}
+                                />
+                                <label htmlFor="bathSelect" className='mx-2 bg-light p-1 rounded'>
+                                    <FontAwesomeIcon icon={faBath} className="text-dark" size="xl"/>
+                                </label>
+                                <Select 
+                                id="bathSelect"
+                                className="ms-2 me-4" 
+                                options={bathOptions}
+                                value={{value: selectedBaths, label: selectedBaths}}
+                                onChange={(event) => {
+                                    setSelectedBaths(event.value)
+                                    console.log(selectedBaths)
+                                }}
+                                />
+                                <label htmlFor="garageSelect" className='mx-2 bg-light p-1 rounded'>
+                                    <FontAwesomeIcon icon={faCar} className="text-dark" size="xl"/>
+                                </label>
+                                <Select 
+                                id="garageSelect"
+                                className="" 
+                                options={garageOptions}
+                                value={{value: selectedGarages, label: selectedGarages}}
+                                onChange={(event) => {
+                                    setSelectedGarages(event.value)
+                                    console.log(selectedGarages)
+                                }}
+                                />
+                            </div>
                         </div>
                     </div>
                 </form>
